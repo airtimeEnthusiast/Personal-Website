7f0e702894a3b9785d492e4b40d772eb706ffa97
airtimeEnthusiast <thewrightguy101@gmail.com>
2024-11-20 12:16:31 -0700

    Updated nav bar for mobile support

--- personal-website/src/Components/Navbar.js
+++ personal-website/src/Components/Navbar.js
@@ -1,66 +1,127 @@
 // src/Components/Navbar.js
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import {
+  FaLinkedin,
+  FaGithub,
+  FaInstagram,
+  FaFileAlt,
+  FaYoutube,
+  FaPhone,
+  FaEnvelope,
+  FaBars,
+  FaTimes,
+} from 'react-icons/fa';
 import './Navbar.css';
-import { FaInstagram, FaLinkedin, FaYoutube, FaGithub, FaFileAlt, FaPhone, FaEnvelope, FaTimes, FaBars } from 'react-icons/fa';
-
-
-// <li><Link to="/projects/Other">Other</Link></li>
 
 const Navbar = () => {
-
   const [isOpen, setIsOpen] = useState(false);
 
   const toggleMenu = () => {
     setIsOpen(!isOpen);
   };
 
-    return (
-      <header className="navbar">
-        <div className="header-top">
+  const closeMenu = () => {
+    setIsOpen(false); // Close menu when a link is clicked
+  };
+
+  return (
+    <header className="navbar">
+      <div className="header-top">
+        {/* Hamburger Icon */}
         <div className="nav-icon" onClick={toggleMenu}>
           {isOpen ? <FaTimes /> : <FaBars />}
         </div>
-        </div>
-        <nav className="nav-container">
-          <ul className="nav-list">
-            <li className="nav-item"><Link to="/">Home</Link></li>
-            <li className="nav-item dropdown">
-              <span>Projects</span>
-              <ul className="dropdown-menu">
-                <li><Link to="/projects/Coding">Coding</Link></li>
-                <li><Link to="https://github.com/airtimeEnthusiast/Blender-Renders">3D Modeling</Link></li>
-              </ul>
-            </li>
-            <li className="nav-item"><Link to="/">Roller Coasters</Link></li>
-            <li className="nav-item"><Link to="/">Mountain Biking</Link></li>
-          </ul>
-        </nav>
-        <div className="social-icons">
-            <a href="https://www.linkedin.com/in/austin-wright/" target="_blank" rel="noopener noreferrer" className="icon">
-              <FaLinkedin />
-            </a>
-            <a href="https://github.com/airtimeEnthusiast" target="_blank" rel="noopener noreferrer" className="icon">
-              <FaGithub />
-            </a>
-            <a href="https://www.instagram.com/awstin_write/profilecard/?igsh=eWVjYzdiZnhlYTdo" target="_blank" rel="noopener noreferrer" className="icon">
-              <FaInstagram />
-            </a>
-            <a href="https://drive.google.com/file/d/1Ky7B_WEVjAgHBU2sqLKrxpyJkqPaU3jW/view?usp=share_link" target="_blank" rel="noopener noreferrer" className="icon">
-            <FaFileAlt />
-            </a>
-            <a href="https://www.youtube.com/@D00rHandleMedia" target="_blank" rel="noopener noreferrer" className="icon">
-              <FaYoutube />
-            </a>
-            <a href="tel:4088883144" target="_blank" rel="noopener noreferrer" className="icon">
-              <FaPhone />
-            </a>
-            <a href="mailto:your-email@example.com" target="_blank" rel="noopener noreferrer" className="icon">
-              <FaEnvelope />
-            </a>
-          </div>
-      </header>
-    );
-  };
-  
-  export default Navbar;
+      </div>
+      <nav className={`nav-container ${isOpen ? 'active' : ''}`}>
+        <ul className="nav-list">
+          <li className="nav-item">
+            <Link to="/" onClick={closeMenu}>
+              Home
+            </Link>
+          </li>
+          <li className="nav-item dropdown">
+            <span>Projects</span>
+            <ul className="dropdown-menu">
+              <li>
+                <Link to="/projects/Coding" onClick={closeMenu}>
+                  Coding
+                </Link>
+              </li>
+              <li>
+                <a
+                  href="https://github.com/airtimeEnthusiast/Blender-Renders"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  onClick={closeMenu}
+                >
+                  3D Modeling
+                </a>
+              </li>
+            </ul>
+          </li>
+          <li className="nav-item">
+            <Link to="/roller-coasters" onClick={closeMenu}>
+              Roller Coasters
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/mountain-biking" onClick={closeMenu}>
+              Mountain Biking
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <div className="social-icons">
+        <a
+          href="https://www.linkedin.com/in/austin-wright/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="icon"
+        >
+          <FaLinkedin />
+        </a>
+        <a
+          href="https://github.com/airtimeEnthusiast"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="icon"
+        >
+          <FaGithub />
+        </a>
+        <a
+          href="https://www.instagram.com/awstin_write/profilecard/?igsh=eWVjYzdiZnhlYTdo"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="icon"
+        >
+          <FaInstagram />
+        </a>
+        <a
+          href="https://drive.google.com/file/d/1Ky7B_WEVjAgHBU2sqLKrxpyJkqPaU3jW/view?usp=share_link"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="icon"
+        >
+          <FaFileAlt />
+        </a>
+        <a
+          href="https://www.youtube.com/@D00rHandleMedia"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="icon"
+        >
+          <FaYoutube />
+        </a>
+        <a href="tel:4088883144" className="icon">
+          <FaPhone />
+        </a>
+        <a href="mailto:your-email@example.com" className="icon">
+          <FaEnvelope />
+        </a>
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
